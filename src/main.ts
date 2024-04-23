e991237838953ca73d34f2d3221c033066f09733
Arafat <arafatmannan2019@gmail.com>
2024-04-23 14:56:01 +0600

    doc updated"

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,24 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const doc = /*html*/`<h2> A backend service for Generate and share Ai Images. </h1>
+  <ol>
+    <li>Generate Image from Differnt model</li>
+    <li>Save Image For your showcase</li>
+    <li>Like, share and comment like a sociel media</li>
+    <li>Create Yor Own group of image</li>
+  </ol>
+  <h3>Thise platfrom is poowerd by:-</h3>
+  <ol class="tech">
+    <li>Nest jS</li>
+    <li>Prisma ORM</li>
+    <li>Cloudinary</li>
+    <li>MongoDB</li>
+  </ol>
+  `
   const config = new DocumentBuilder()
     .setTitle('Fusion AI server 2.0')
-    .setDescription('A server for Ai generated image storeage and share as social media. It is made by using nest js which uses express under. For ORM it uses prisma and for database it uses Mongodb')
+    .setDescription(doc)
     .setVersion('1.0')
     .addBearerAuth()
     .build();
